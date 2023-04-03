4c5b8307f76edf2a059bfb8418ea935fe2afd42e
JaehwanLee <96andrew@naver.com>
2023-04-04 00:00:45 +0900

    Update dataload.py

--- dataset/dataload.py
+++ dataset/dataload.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# Dataload and split
+train_df = pd.read_csv('./train.csv', encoding='cp949', indexl_col='compound')
+test_df = pd.read_csv('./test.csv', encoding='cp949', indexl_col='compound')
+
+y_tr = train_df.pop('target')
+y_ts = test_df.pop('target')
+
+x_tr, x_ts = train_df, test_df
+
+# Delete columns with unique value
 
